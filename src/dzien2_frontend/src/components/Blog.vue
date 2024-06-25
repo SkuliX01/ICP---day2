54c59d1b7c28749bee98e7dec31512798546e283
SkuliX01 <bartosz.krawczyk34@gmail.com>
2024-06-25 19:52:29 +0200

    Created blog component

---
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button @click="pobierzWpisy">Odczytaj Wpisy</button>
+        <h1>siema blog</h1>
+        {{ wpisy }}
+        <input type="text" v-model="nowyBlog">
+        <button @click="dodajWpis">Dodaj nowy wpis</button>
+    </div>
+</template>
+
+<script>
+
+    import { dzien2_backend } from 'declarations/dzien2_backend/index';
+
+    export default {
+        data() {
+            return {
+                wpisy: [],
+                nowyBlog: ""
+            }
+        },
+        methods: {
+            async pobierzWpisy(){
+                this.wpisy = await dzien2_backend.odczytaj_wpisy()
+            },
+            async dodajWpis(){
+                await dzien2_backend.dodaj_wpis(this.nowyBlog)
+            }
+        }
+    }
+</script>
